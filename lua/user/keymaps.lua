e455028ac7c6775ef85f457e0b8125fc9782ec51
Aaron Graham <aaron.graham@podium.com>
2022-03-12 22:36:29 -0800

    fix keymap for moving lines in normal mode

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -38,8 +38,8 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-keymap("v", "∆", "<Esc>:m .+1<CR>==gi", opts)
-keymap("v", "˚", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "∆", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "˚", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
 -- Press jk fast to enter
