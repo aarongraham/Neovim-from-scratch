f87aaac7459458525cd50a70111b2cbecae22de3
Aaron Graham <aaron.graham@podium.com>
2022-03-15 20:50:35 -0700

    fix prettier formatting and add shortcuts

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,9 +11,21 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({ extra_args = { } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.mix,
 		formatting.stylua,
+    diagnostics.eslint
     -- diagnostics.flake8
 	},
+  on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+        end
+    end,
 })
