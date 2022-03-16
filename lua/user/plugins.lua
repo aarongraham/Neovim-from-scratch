42df100b874a149a81481570a621642ab850607a
Aaron Graham <aaron.graham@podium.com>
2022-03-15 20:07:23 -0700

    add ts playground

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,6 +96,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use "nvim-treesitter/playground"
   use "JoosepAlviste/nvim-ts-context-commentstring"
 
   -- Git
