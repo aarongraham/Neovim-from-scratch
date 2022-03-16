23fbe7988e30e98e946a5b11d999653785cba7ad
Aaron Graham <aaron.graham@podium.com>
2022-03-15 20:08:13 -0700

    save on focus change

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -44,3 +44,10 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- Needed for some neovim gui programs
+vim.cmd "set guifont=\"JetBrains Mono NerdFont\""
+
+-- Save on focus lost and buffer switch
+vim.cmd "au FocusLost * silent! wa"
+vim.cmd "set autowriteall"
