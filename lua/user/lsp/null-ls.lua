0d6b70fe295e055a5574979620f402dbaf0f47d0
Aaron Graham <aaron.graham@podium.com>
2022-04-10 21:50:51 -0700

    fix warning when starting elixirls lsp

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -21,11 +21,11 @@ null_ls.setup({
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
 			vim.cmd([[
-		          augroup LspFormatting
-		              autocmd! * <buffer>
-		              autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
-		          augroup END
-		          ]])
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
+            augroup END
+            ]])
 		end
 	end,
 })
