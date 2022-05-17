a0fd06644d4108aba36016570c58594e2add1344
Aaron Graham <aaron.graham@podium.com>
2022-05-17 10:27:38 -0700

    extra neotree cconfig

--- lua/user/neotree.lua
+++ lua/user/neotree.lua
@@ -136,7 +136,7 @@ neo_tree.setup({
 	nesting_rules = {},
 	filesystem = {
 		filtered_items = {
-			visible = false, -- when true, they will just be displayed differently than normal items
+			visible = true, -- when true, they will just be displayed differently than normal items
 			hide_dotfiles = true,
 			hide_gitignored = true,
 			hide_hidden = true, -- only works on Windows for hidden files/directories
@@ -147,11 +147,11 @@ neo_tree.setup({
 				--"*.meta"
 			},
 			never_show = { -- remains hidden even if visible is toggled to true
-				--".DS_Store",
-				--"thumbs.db"
+				".DS_Store",
+				"thumbs.db",
 			},
 		},
-		follow_current_file = false, -- This will find and focus the file in the active buffer every
+		follow_current_file = true, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
 		group_empty_dirs = false, -- when true, empty folders will be grouped together
 		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -159,7 +159,7 @@ neo_tree.setup({
 		-- "open_current",  -- netrw disabled, opening a directory opens within the
 		-- window like netrw would, regardless of window.position
 		-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-		use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+		use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
 		-- instead of relying on nvim autocmd events.
 		window = {
 			mappings = {
